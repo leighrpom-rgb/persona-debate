17fd4dc1ee87872738d11846802b8b2ed56a2d32
leighrpom-rgb <leighrpom@gmail.com>
2026-02-24 19:36:39 -0800

    Demo only: intentionally commit exposed API key for Render static deploy
    Demo only: intentionally commit exposed API key for Render static deploy

---
+++ config-keys.js
@@ -0,0 +1,5 @@
+// This file contains your API key
+// It is gitignored and will NOT be pushed to GitHub
+const CONFIG_KEYS = {
+  OPENAI_API_KEY: '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'
+};
